8df98c2303ffc89dc15222bc67adcc50a5aa5ee1
Udana1 <udanasrimal123@gmail.com>
2026-05-09 16:57:16 +0530

    Added popup for successfull messages

--- members/add.php
+++ members/add.php
@@ -80,11 +80,20 @@ if(isset($_POST['submit'])){
 
                 </div>
 
-                <?php
-                if($message != ""){
-                    echo "<div class='alert alert-info'>$message</div>";
-                }
-                ?>
+                    <?php if($message != ""){ ?>
+
+                    <   div class="alert alert-success alert-dismissible fade show"role="alert">
+
+                    <?php echo $message; ?>
+
+                    <button type="button"
+                    class="btn-close"
+                    data-bs-dismiss="alert">
+                    </button>
+
+                </div>
+
+<?php } ?>
 
                 <form method="POST">
 
@@ -172,6 +181,6 @@ if(isset($_POST['submit'])){
     </div>
 
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
