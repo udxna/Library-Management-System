8f2c992120562af4ce87c2f25586cfde4c7c2b99
Nimsara07 <2024t02218@stu.cmb.ac.lk>
2026-05-11 22:21:57 +0530

    Create navigation bar code

---
+++ includes/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">LMS</a>
+
+    <div>
+      <a href="dashboard.php" class="btn btn-light btn-sm">Dashboard</a>
+      <a href="view_users.php" class="btn btn-warning btn-sm">Users</a>
+      <a href="logout.php" class="btn btn-danger btn-sm">Logout</a>
+    </div>
+
+  </div>
+</nav>
