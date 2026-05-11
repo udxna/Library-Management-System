05e47f6cb895c76699e1301824be4d7146cfc2d4
Nimsara07 <2024t02218@stu.cmb.ac.lk>
2026-05-11 22:19:48 +0530

    Create the Header

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Library Management System</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="../assets/css/style.css">
+
+</head>
+<body>
