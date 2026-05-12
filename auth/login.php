2d244aca3654a045b2974163ab89d7629dde3fd9
Nimsara07 <2024t02218@stu.cmb.ac.lk>
2026-05-13 01:02:23 +0530

    Change code

--- auth/login.php
+++ auth/login.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Modern Login UI</title>
+    <title>LMS Login</title>
 
     <?php if ($error): ?>
         <p style="color:red;"><?= htmlspecialchars($error) ?></p>
