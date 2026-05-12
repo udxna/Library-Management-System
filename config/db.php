1f46343a38eb59deffe2eb70f42664bbcb0e330c
Nimsara07 <2024t02218@stu.cmb.ac.lk>
2026-05-12 16:18:19 +0530

    Update the PHP Code

--- config/db.php
+++ config/db.php
@@ -1,14 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "library_system";
+$conn = mysqli_connect("localhost","root","","library_system");
 
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-    die("Database Connection Failed: " . mysqli_connect_error());
+if(!$conn){
+    die("Connection Failed: " . mysqli_connect_error());
 }
 
 ?>
