1fb705fa0471d94afa6a05bdb5bdc2395c716f77
Nimsara07 <2024t02218@stu.cmb.ac.lk>
2026-05-12 23:52:31 +0530

    Update the code

--- auth/view_users.php
+++ auth/view_users.php
@@ -14,43 +14,6 @@ $result = mysqli_query($conn,$sql);
 <?php include '../includes/header.php'; ?>
 <?php include '../includes/navbar.php'; ?>
 
-<div class="container mt-5">
-
-<div class="card shadow p-4">
-
-<h2 class="mb-4">User List</h2>
-
-<table class="table table-bordered table-hover">
-
-<tr class="table-dark">
-<th>ID</th>
-<th>User ID</th>
-<th>First Name</th>
-<th>Last Name</th>
-<th>Username</th>
-<th>Email</th>
-<th>Actions</th>
-</tr>
-
-<?php while($row = mysqli_fetch_assoc($result)){ ?>
-
-<tr>
-<td><?php echo $row['id']; ?></td>
-<td><?php echo $row['userid']; ?></td>
-<td><?php echo $row['firstname']; ?></td>
-<td><?php echo $row['lastname']; ?></td>
-<td><?php echo $row['username']; ?></td>
-<td><?php echo $row['email']; ?></td>
-
-<td>
-<a href="update_user.php?id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
-
-<a href="delete_user.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">
-Delete
-</a>
-</td>
-</tr>
-
 <?php } ?>
 
 </table>
