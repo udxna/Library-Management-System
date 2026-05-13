9be1b02d66aeb36857e149d39d7721ee1117307f
2004dhananjana-glitch <2004dhananjana@gmail.com>
2026-05-13 15:36:14 +0530

    update the code

--- borrow/borrow.php
+++ borrow/borrow.php
@@ -8,10 +8,10 @@ include("../config/db.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
-<body>
+<body class=container mt-5>
     
 </body>
 </html>
