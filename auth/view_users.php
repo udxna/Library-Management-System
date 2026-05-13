38988e2c561e62d868f9acf9b30a0136da82531b
chamodgamachchige <chamodnimsar.mail@gmail.com>
2026-05-14 03:08:05 +0530

    Fix Bug

--- auth/view_users.php
+++ auth/view_users.php
@@ -370,7 +370,7 @@ $result = mysqli_query($conn, $sql);
       color:#000;
       border:1px solid #ccc;
       border-radius:10px;
-      padding:6px 20px;
+      padding:20px;
       font-weight:100;
       
       
@@ -381,7 +381,7 @@ $result = mysqli_query($conn, $sql);
       color:#fff;
       border:1px solid #ccc;
       border-radius:10px;
-      padding:6px 20px;
+      padding: 20px;
       font-weight:100;
       
     }
