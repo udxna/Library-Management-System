d8972ae0bd1bff3980df0166d8a2bbde9e2a677b
2004dhananjana-glitch <2004dhananjana@gmail.com>
2026-05-14 01:05:24 +0530

    add header for page

--- borrow/edit_borrow.php
+++ borrow/edit_borrow.php
@@ -21,7 +21,7 @@ if (isset($_POST['update'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body class="container mt-5">
-    
+    <h2>Update Borrow Status</h2>
    
 </body>
 </html>
