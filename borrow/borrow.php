eb5f791e9310790e190efabbf224fe95f442ba6c
2004dhananjana-glitch <2004dhananjana@gmail.com>
2026-05-13 15:37:13 +0530

    add h2

--- borrow/borrow.php
+++ borrow/borrow.php
@@ -12,6 +12,7 @@ include("../config/db.php");
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body class=container mt-5>
+    <h2>Borrow Book Details</h2>
     
 </body>
 </html>
