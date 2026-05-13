c24a198e553aa3abd868b5675b4280693923ff94
Udana1 <udanasrimal123@gmail.com>
2026-05-13 12:23:20 +0530

    fixed the dashboard error

--- auth/login.php
+++ auth/login.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $_SESSION['username']  = $user['username'];
             $_SESSION['firstname'] = $user['firstname'];
             $_SESSION['lastname']  = $user['lastname'];
-            header("Location: dashboard.php");
+            header("Location: ../dashboard/index.php");
             exit();
         } else {
             $error = "Invalid username or password.";
