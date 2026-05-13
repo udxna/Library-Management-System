9592376aeee7c797ab030b3d776bd5a0f1ef48c4
chamodgamachchige <chamodnimsar.mail@gmail.com>
2026-05-13 18:50:33 +0530

    Update the Code

--- auth/delete_user.php
+++ auth/delete_user.php
@@ -1,16 +1,19 @@
 <?php
 session_start();
-include '../config/db.php';
+require_once '../config/db.php';
 
-if(!isset($_SESSION['username'])){
+if (!isset($_SESSION['username'])) {
     header("Location: login.php");
+    exit();
 }
 
-$id = $_GET['id'];
-
-$sql = "DELETE FROM users WHERE id='$id'";
-
-mysqli_query($conn,$sql);
+if (isset($_GET['id']) && $_GET['id'] !== '') {
+    $user_id = $_GET['id'];
+    $stmt = mysqli_prepare($conn, "DELETE FROM `user` WHERE user_id = ?");
+    mysqli_stmt_bind_param($stmt, "s", $user_id);
+    mysqli_stmt_execute($stmt);
+}
 
 header("Location: view_users.php");
+exit();
 ?>
